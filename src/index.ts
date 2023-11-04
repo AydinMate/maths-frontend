8d58cde6f0a0e21acf65fd2845d625c8e0d2df1f
Aydin Elhan <aydinmate@protonmail.com>
2023-11-04 18:34:34 +1100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+const problem = document.getElementsByClassName('problem')[0];
+
+problem.addEventListener('click', () => {
+  console.log('clicked');
+});
+
+async function fetchData() {
+  try {
+    const response = await fetch('http://localhost:5288/get-arithmetic-mp');
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error('Fetch error:', error);
+  }
+}
+
+
+fetchData();
