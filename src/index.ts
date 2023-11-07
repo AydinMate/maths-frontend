0a22fdda17bfc201ccdd795bea51d9086ad4f5bb
Aydin Elhan <aydinmate@protonmail.com>
2023-11-07 19:46:14 +1100

    add scss

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,52 @@
 import { ArithmeticQuestion } from './types';
 
-var problem = document.querySelector('.problem');
-var data: ArithmeticQuestion;
+let loader = document.querySelector('.loader') as HTMLElement;
+
+let questionElement = document.querySelector('.question') as HTMLElement;
+let problemElement = document.querySelector('.problem') as HTMLElement;
+let optionsElement = document.querySelector('.options') as HTMLElement;
+
+let data: ArithmeticQuestion = {
+  operation: '',
+  firstNumber: 0,
+  secondNumber: 0,
+  answer: 0,
+  options: [],
+};
 
 async function fetchData() {
   try {
     const response = await fetch('http://localhost:5288/get-arithmetic-mp');
 
     if (!response.ok) {
+      setData(false);
       throw new Error('Network response was not ok');
+    } else {
+      data = await response.json();
+
+      problemElement.innerHTML = `${data.firstNumber} ${data.operation} ${data.secondNumber}`;
+
+      data.options.forEach((optionValue) => {
+        const optionElement = document.createElement('button');
+        optionElement.className = 'option';
+        optionElement.textContent = optionValue.toString();
+        optionsElement.appendChild(optionElement);
+      });
+
+      setData(true);
     }
-
-    data = await response.json();
-
-    problem &&
-      (await (problem.innerHTML = `${data.firstNumber} ${data.operation} ${data.secondNumber}`));
   } catch (error) {
     console.error('Fetch error:', error);
+  }
+}
+
+function setData(showData: boolean) {
+  if (showData) {
+    questionElement.style.display = 'block';
+    loader.style.display = 'none';
+  } else {
+    questionElement.style.display = 'none';
+    loader.style.display = 'block';
   }
 }
 
