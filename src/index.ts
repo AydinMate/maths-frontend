f403e9e02a374f3a0ef92feb2b828dcd4fd976e7
Aydin Elhan <200030355@student.kangan.edu.au>
2023-11-09 15:07:26 +1100

    answers working

--- src/index.ts
+++ src/index.ts
@@ -1,68 +1,94 @@
-import { ArithmeticQuestion } from './types';
+import { ArithmeticQuestion } from "./types";
 
-let loader = document.querySelector('.loader') as HTMLElement;
+let loader = document.querySelector(".loader") as HTMLElement;
 
-let questionElement = document.querySelector('.question') as HTMLElement;
-let problemElement = document.querySelector('.problem') as HTMLElement;
-let optionsElement = document.querySelector('.options') as HTMLElement;
+let backgroundElement = document.querySelector(".container") as HTMLElement;
+let questionElement = document.querySelector(".question") as HTMLElement;
+let problemElement = document.querySelector(".problem") as HTMLElement;
+let allOptions = document.querySelectorAll(".option");
 
 let data: ArithmeticQuestion = {
-  operation: '',
+  operation: "",
   firstNumber: 0,
   secondNumber: 0,
   answer: 0,
   options: [],
 };
 
-function checkOption (option: number, answer: number) {
+function checkOption(option: number, answer: number) {
   if (option === answer) {
     return true;
   } else {
     return false;
   }
-};
-
-
+}
 
 function setData(showData: boolean) {
   if (showData) {
-    questionElement.style.display = 'block';
-    loader.style.display = 'none';
+    questionElement.style.display = "block";
+    loader.style.display = "none";
   } else {
-    questionElement.style.display = 'none';
-    loader.style.display = 'block';
+    questionElement.style.display = "none";
+    loader.style.display = "block";
   }
+}
+
+function checkAnswer(optionValue: number) {
+  const isCorrect = optionValue === data.answer;
+  if (isCorrect) {
+    backgroundElement.style.backgroundColor = "green";
+  } else {
+    backgroundElement.style.backgroundColor = "red";
+  }
+  setTimeout(() => {
+    backgroundElement.style.backgroundColor = "black";
+  }, 300);
+}
+
+function addEventListeners() {
+  allOptions.forEach((option) => {
+    option.addEventListener("click", () => {
+      const optionString = option.textContent;
+      let optionValue;
+
+      if (optionString) {
+        optionValue = parseInt(optionString);
+        checkAnswer(optionValue);
+      }
+
+      fetchData();
+    });
+  });
+}
+
+function updateOptions() {
+  allOptions.forEach((option, index) => {
+    const optionValue = data.options[index];
+    if (optionValue != null) {
+      option.textContent = optionValue.toString();
+    }
+  });
 }
 
 async function fetchData() {
   try {
-    const response = await fetch('http://localhost:5288/get-arithmetic-mp');
+    const response = await fetch("http://localhost:5288/get-arithmetic-mn");
 
     if (!response.ok) {
       setData(false);
-      throw new Error('Network response was not ok');
+      throw new Error("Network response was not ok");
     } else {
       data = await response.json();
 
       problemElement.innerHTML = `${data.firstNumber} ${data.operation} ${data.secondNumber}`;
 
-      data.options.forEach((optionValue) => {
-        const optionElement = document.createElement('button');
-        optionElement.className = 'option';
-        optionElement.textContent = optionValue.toString();
-        optionsElement.appendChild(optionElement);
-
-        optionElement.addEventListener('click', () => {
-          const res = checkOption(optionValue, data.answer);
-          console.log(res);
-        });
-      });
-
+      updateOptions();
       setData(true);
     }
   } catch (error) {
-    console.error('Fetch error:', error);
+    console.error("Fetch error:", error);
   }
 }
 
+addEventListeners();
 fetchData();
